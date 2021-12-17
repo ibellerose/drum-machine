013bdb3af71d5bc4f55535000f67cb7dc25ec03c
Ian Bellerose <ianbellerose@gmail.com>
2021-12-16 21:30:47 -0500

    added keyboard press function to play drum

--- drum-machine.js
+++ drum-machine.js
@@ -10,72 +10,145 @@ const Display = (props) =>{
 };
 
 
+//classes
 class Buttons extends React.Component{
     constructor(props){
         super(props);
+        
+        this.state = {drumName: ""};
     }
 
     playAudio = (id) => {
         var audio = document.getElementById(id);
+        
         if(audio.paused){
             audio.play();
         }
         else{
             audio.currentTime = 0;
         }
+
+        switch (id) {
+            case 'Q': 
+                this.setState({drumName: 'Tribal Drum'});
+                break;
+            case 'W':
+                this.setState({drumName: 'Losing Drum'});
+                break;
+            case 'E':
+                this.setState({drumName: 'Percussion Drum'});
+                break;
+            case 'A':
+                this.setState({drumName: 'Drum Roll'});
+                break;
+            case 'S':
+                this.setState({drumName: 'Hand Drum'});
+                break;
+            case 'D':
+                this.setState({drumName: 'Joke Drum'});
+                break;
+            case 'Z':
+                this.setState({drumName: 'Bass Drum'});
+                break;
+            case 'X':
+                this.setState({drumName: 'Short Bass Drum'});
+                break;
+            case 'C':
+                this.setState({drumName: 'Futuristic Drum'});
+                break;
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeypress);
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeypress);
+    }
+
+    
+    handleKeypress = e => {
+        var key = e.key;
+
+        if(key == 'q' || key == 'Q'){
+            this.playAudio('Q');
+        }
+        else if(key == 'w' || key == 'W'){
+            this.playAudio('W');
+        }
+        else if(key == 'e' || key == 'E'){
+            this.playAudio('E');
+        }
+        else if(key == 'a' || key == 'A'){
+            this.playAudio('A');
+        }
+        else if(key == 's' || key == 'S'){
+            this.playAudio('S');
+        }
+        else if(key == 'd' || key == 'D'){
+            this.playAudio('D');
+        }
+        else if(key == 'z'  || key == 'Z'){
+            this.playAudio('Z');
+        }
+        else if(key == 'x' || key == 'X'){
+            this.playAudio('X');
+        }
+        else if(key == 'c' || key == 'C'){
+            this.playAudio('C');
+        }
+
     }
 
     render(){
         return(
-            <div class="button-wrapper">
-            <button class="drum-pad" onClick={()=>this.playAudio('Q')}>Q
-                <audio class="clip" id="Q" src="https://assets.mixkit.co/sfx/preview/mixkit-tribal-dry-drum-558.mp3"></audio>
-            </button>
+            <div onKeyDown={this.handleKeypress}>
+                <div class="button-wrapper">
+                <button class="drum-pad"  onClick={()=>this.playAudio('Q')}>Q
+                    <audio class="clip" id="Q" src="https://assets.mixkit.co/sfx/preview/mixkit-tribal-dry-drum-558.mp3"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('W')}>W
-                <audio src="losing-drums.wav" class="clip" id="W"></audio>
-            </button>
+                <button class="drum-pad"  onClick={()=>this.playAudio('W')}>W
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-losing-drums-2023.mp3" class="clip" id="W"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('E')}>E
-                <audio src="drum-and-percussion.wav" class="clip" id="E"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('E')}>E
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-drum-and-percussion-545.mp3" class="clip" id="E"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('A')}>A
-                <audio src="toy-drums-and-bell-ding.wav" class="clip" id="A"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('A')}>A
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-toy-drums-and-bell-ding-560.mp3" class="clip" id="A"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('S')}>S
-                <audio src="hand-tribal-drum.wav" class="clip" id="S"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('S')}>S
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-hand-tribal-drum-562.mp3" class="clip" id="S"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('D')}>D
-                <audio src="joke-drums.wav" class="clip" id="D"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('D')}>D
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-bad-joke-drums-2893.mp3" class="clip" id="D"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('Z')}>Z
-                <audio src="drum-bass-hit.wav" class="clip" id="Z"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('Z')}>Z
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-blockbuster-trailer-bass-drum-553.mp3" class="clip" id="Z"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('X')}>X
-                <audio src="short-bass-hit.wav" class="clip" id="X"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('X')}>X
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-short-bass-hit-2299.mp3" class="clip" id="X"></audio>
+                </button>
 
-            <button class="drum-pad" onClick={()=>this.playAudio('C')}>C
-                <audio src="futuristic-bass-hit.wav" class="clip" id="C"></audio>
-            </button>
+                <button class="drum-pad" onClick={()=>this.playAudio('C')}>C
+                    <audio src="https://assets.mixkit.co/sfx/preview/mixkit-futuristic-bass-hit-2303.mp3" class="clip" id="C"></audio>
+                </button>
+            </div>
+                
+                <h1 id="text">{this.state.drumName}</h1>
+
         </div>
         )
     }
 
 }
 
-//functions
-/*
-function = playAudio => () {
-    new Audio(audio).play();
-    //document.getElementById('q').play();
-}
-*/
 
 class DrumMachine extends React.Component{
     constructor(props){
@@ -88,7 +161,6 @@ class DrumMachine extends React.Component{
             <div id="machine">
                 <Display />
                 <Buttons />
-
             </div>
         )
     }
